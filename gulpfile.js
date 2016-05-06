7f9675180ae2ceeac810b2151fbac590bec6cf33
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-05-06 16:15:27 +0100

    linking govuk_frontend_toolkit to main scss file

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ gulp.task('css', function() {
   return gulp.src('assets/stylesheets/*.scss')
     .pipe(
       sass( {
-        includePaths: ['src/assets/stylesheets'],
+        includePaths: [
+          'src/assets/stylesheets',
+          'node_modules/govuk_frontend_toolkit/stylesheets'
+        ],
         errLogToConsole: true
       } ) )
     .pipe( gulp.dest('dist/public/stylesheets/') );
