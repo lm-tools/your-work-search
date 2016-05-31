03b4b03e2cb4fd38483c1b06254047cb3bd100a7
Gareth Visagie <gareth@gjvis.com>
2016-05-31 12:18:48 +0100

    Datepickers:native on mobile, JQueryUI on desktop

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist/public')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery-ui/themes/base/minified')));
 app.use(express.static(path.join(__dirname,
   'vendor', 'govuk_template_mustache_inheritance', 'assets')));
 
