4d5141fbdfac821af45c6660ae7f1d08fbac7f78
Gareth Visagie <gareth@gjvis.com>
2016-06-03 14:40:23 +0100

    Update DashboardPage pageobject url to be correct

--- test/common/page_objects/DashboardPage.js
+++ test/common/page_objects/DashboardPage.js
@@ -1,6 +1,6 @@
 const DashboardPage = function DashboardPage(browser) {
   this.browser = browser;
-  this.visit = (id) => browser.visit(`/?id=${id}`);
+  this.visit = (id) => browser.visit(`/${id}`);
 
   this.clickAddJobButton = () => browser.click('a.button');
   this.jobList = () => browser.text('ul h4');
