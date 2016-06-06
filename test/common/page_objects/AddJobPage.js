8787b9620a568f24507615381208d3e29ab39924
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-06-06 16:31:31 +0100

    Replace css id selectors with name selectors

--- test/common/page_objects/AddJobPage.js
+++ test/common/page_objects/AddJobPage.js
@@ -3,33 +3,33 @@ const AddJobPage = function AddJobPage(browser) {
 
   this.fillJobApplication = (data) =>
     browser
-      .fill('#job-title', data.title)
-      .fill('#job-employer', data.employer)
+      .fill('[name="title"]', data.title)
+      .fill('[name="employer"]', data.employer)
       .choose(`#job-sourceType-${data.sourceType}`)
-      .fill('#job-sourceUrl', data.sourceUrl)
+      .fill('[name="sourceUrl"]', data.sourceUrl)
       .choose(`#job-rating-${data.rating}`)
-      .fill('#job-deadline', data.deadline)
+      .fill('[name="deadline"]', data.deadline)
       .pressButton('input[type=submit]');
 
   this.getValidationError = () => browser.text('#validation-errors');
 
   this.visit = (accountId) => browser.visit(`/${accountId}/jobs/new`);
 
-  this.employerFieldValue = () => browser.field('#job-employer').value;
+  this.employerFieldValue = () => browser.field('[name="employer"]').value;
   this.chooseSourceType = (sourceType) => browser.choose(`#job-sourceType-${sourceType}`);
   this.isSourceUrlHidden = () =>
     browser.query('#job-sourceUrl-group').className.split(/\s+/).includes('js-hidden');
   this.formValues = () =>
     ({
-      title: browser.field('#job-title').value,
-      employer: browser.field('#job-employer').value,
+      title: browser.field('[name="title"]').value,
+      employer: browser.field('[name="employer"]').value,
       sourceType: browser.field('[name="sourceType"][checked]').value,
-      sourceUrl: browser.field('#job-sourceUrl').value,
+      sourceUrl: browser.field('[name="sourceUrl"]').value,
       rating: browser.field('[name="rating"][checked]').value,
-      deadline: browser.field('#job-deadline').value,
+      deadline: browser.field('[name="deadline"]').value,
     });
-  this.fillDeadline = deadline => browser.fill('#job-deadline', deadline);
-  this.fillTitle = title => browser.fill('#job-title', title);
+  this.fillDeadline = deadline => browser.fill('[name="deadline"]', deadline);
+  this.fillTitle = title => browser.fill('[name="title"]', title);
   this.submit = () => browser.pressButton('input[type=submit]');
 };
 
