40fd4bd07f965d3eff7ca89c57aed84073f25960
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-06-06 15:45:36 +0100

    code style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,12 @@
-$(document).ready(() => {
-  // Use GOV.UK selection-buttons.js to set selected
-  // and focused states for block labels
-  const $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
-  // eslint-disable-next-line no-new
-  new GOVUK.SelectionButtons($blockLabels);
-});
+// Use GOV.UK selection-buttons.js to set selected
+// and focused states for block labels
+const $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
+// eslint-disable-next-line no-new
+new GOVUK.SelectionButtons($blockLabels);
 
 
 // add-a-job page
-$(document).ready(() => {
-  $('label[for="job-sourceType-inPerson"]')
-    .click(() => $('#job-sourceUrl-group').addClass('js-hidden'));
-  $('label[for="job-sourceType-online"]')
-    .click(() => $('#job-sourceUrl-group').removeClass('js-hidden'));
-});
+$('label[for="job-sourceType-inPerson"]')
+  .click(() => $('#job-sourceUrl-group').addClass('js-hidden'));
+$('label[for="job-sourceType-online"]')
+  .click(() => $('#job-sourceUrl-group').removeClass('js-hidden'));
