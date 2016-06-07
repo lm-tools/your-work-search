d64cc1fad94ddcbed6ef4d52d0e3a738338d7186
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-06-07 11:27:36 +0100

    Allows date in input type='date' wiring format 'yyyy-mm-dd'

--- test/integration/add-job-page.spec.js
+++ test/integration/add-job-page.spec.js
@@ -26,18 +26,24 @@ describe('Add a job page', () => {
         .then(() => expect(addJobPage.formValues()).to.eql(form))
     );
 
-    it('should fail incorrect deadline format', () =>
-      promise(() => addJobPage.fillTitle('Some job title'))
-        .then(() => addJobPage.fillDeadline('abc'))
-        .then(() => addJobPage.submit())
-        .then(() => expect(addJobPage.getValidationError())
-          .to.equal('Deadline should be in "dd/mm/yyyy" format'))
-    );
-
-    ['21/10/2016', ''].forEach((deadline) => {
-      it(`should not fail deadline validation for '${deadline}' `, () =>
+    ['abc', '2016-21-12'].forEach(deadline => {
+      it(`should fail incorrect deadline format for date '${deadline}'`, () =>
         promise(() => addJobPage.fillTitle('Some job title'))
           .then(() => addJobPage.fillDeadline(deadline))
+          .then(() => addJobPage.submit())
+          .then(() => expect(addJobPage.getValidationError())
+            .to.equal('Deadline should be in "dd/mm/yyyy" format'))
+      );
+    });
+
+    [
+      { name: 'desktop format "dd/mm/yyyy"', date: '21/10/2016' },
+      { name: 'mobile format "yyyy-mm-dd"', date: '2016-12-21' },
+      { name: 'empty date', date: '' },
+    ].forEach(scenario => {
+      it(`should not fail deadline validation for '${scenario.name}' `, () =>
+        promise(() => addJobPage.fillTitle('Some job title'))
+          .then(() => addJobPage.fillDeadline(scenario.date))
           .then(() => addJobPage.submit())
           .then(() => expect(addJobPage.getValidationError()).to.equal(''))
       );
