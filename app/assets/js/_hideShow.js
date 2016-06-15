b1c07f1b534e3fa6da6ee9f29173bf421873f386
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-06-15 15:42:20 +0100

    Better name for visibility toggle

--- app/assets/js/_hideShow.js
+++ app/assets/js/_hideShow.js
@@ -1,9 +1,9 @@
 const $ = require('jquery');
 
-$('[data-toggle]').on('click', function (el) {
+$('[data-toggle-visibility-for]').on('click', function (el) {
   const $currentElement = $(el.target);
   const $icon = $currentElement.find('i');
-  const $target = $($currentElement.data().toggle);
+  const $target = $($currentElement.data('toggle-visibility-for'));
   $icon.toggleClass('fa-caret-right');
   $icon.toggleClass('fa-caret-down');
   $target.toggleClass('js-hidden');
