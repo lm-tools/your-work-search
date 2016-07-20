e039b707c3cb13438523a462beef6afca751dbed
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-07-20 10:37:18 +0100

    Logs error during health check

--- app/controllers/health-check-controller.js
+++ app/controllers/health-check-controller.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = new express.Router();
 const db = require('../db');
+const logger = require('winston');
 
 router.get('', (req, res) =>
-  db.knex.raw('select 1+1 as result').then(() =>
-    res.json({
-      status: 'ok',
-      database: 'up',
+  db.knex.raw('select 1+1 as result')
+    .then(() =>
+      res.json({
+        status: 'ok',
+        database: 'up',
+      }))
+    .catch((e) => {
+      logger.error('Database is down\n', e);
+      res.status(500).json({
+        status: 'failure',
+        database: 'down',
+      });
     })
-  ).catch(() =>
-    res.status(500).json({
-      status: 'failure',
-      database: 'down',
-    })));
+);
 
 module.exports = router;
