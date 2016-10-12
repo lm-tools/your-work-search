df1b73c704f81869a2ebb6e8924b4cee72233885
nathancashmore <nathancashmore@ucds.email>
2016-10-12 18:16:43 +0100

    Update seeds based on feedback

--- db/seeds/filter/jobs_seed.js
+++ db/seeds/filter/jobs_seed.js
@@ -8,39 +8,39 @@ const threeMonths = moment.duration(3, 'M');
 exports.seed = function (knex) {
   return knex('jobs').del() // Deletes ALL existing entries
     .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '100',
-        accountId: 'FILTER',
-        title: 'C-',
-        employer: 'X-',
-        updated_at: moment().subtract(month).add(day),
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '101',
-        accountId: 'FILTER',
-        title: 'A-',
-        employer: 'Z-',
-        updated_at: moment().subtract(week).add(day),
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '102',
-        accountId: 'FILTER',
-        title: 'B-',
-        employer: 'Y-',
-        updated_at: moment().subtract(fortnight).add(day),
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '103',
-        accountId: 'FILTER',
-        title: 'D-',
-        employer: 'W-',
-        updated_at: moment().subtract(threeMonths),
-      });
+      return knex('jobs').insert([
+        {
+          id: '100',
+          accountId: 'FILTER',
+          title: 'C-',
+          employer: 'X-',
+          status: 'interested',
+          updated_at: moment().subtract(month).add(day),
+        },
+        {
+          id: '101',
+          accountId: 'FILTER',
+          title: 'A-',
+          employer: 'Z-',
+          status: 'interested',
+          updated_at: moment().subtract(week).add(day),
+        },
+        {
+          id: '102',
+          accountId: 'FILTER',
+          title: 'B-',
+          employer: 'Y-',
+          status: 'interested',
+          updated_at: moment().subtract(fortnight).add(day),
+        },
+        {
+          id: '103',
+          accountId: 'FILTER',
+          title: 'D-',
+          employer: 'W-',
+          status: 'interested',
+          updated_at: moment().subtract(threeMonths),
+        },
+      ]);
     });
 };
--- db/seeds/sort/jobs_seed.js
+++ db/seeds/sort/jobs_seed.js
@@ -1,62 +1,61 @@
 const moment = require('moment');
-const day = moment.duration(1, 'd');
+const oneDay = moment.duration(1, 'd');
+const twoDays = moment.duration(2, 'd');
+const threeDays = moment.duration(3, 'd');
+const fourDays = moment.duration(4, 'd');
 
 exports.seed = function (knex) {
   return knex('jobs').del() // Deletes ALL existing entries
     .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '100',
-        accountId: 'SORT',
-        title: 'A-',
-        employer: 'A-',
-        created_at: moment().subtract(day),
-        updated_at: moment().subtract(day),
-        deadline: moment().add(day),
-        rating: '1',
-        status: 'interested',
-        status_sort_index: 0,
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '101',
-        accountId: 'SORT',
-        title: 'B-',
-        employer: 'B-',
-        created_at: moment().subtract(day).subtract(day),
-        updated_at: moment().subtract(day).subtract(day),
-        deadline: moment().add(day).add(day),
-        rating: '2',
-        status: 'applied',
-        status_sort_index: 1,
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '102',
-        accountId: 'SORT',
-        title: 'C-',
-        employer: 'C-',
-        created_at: moment().subtract(day).subtract(day).subtract(day),
-        updated_at: moment().subtract(day).subtract(day).subtract(day),
-        deadline: moment().add(day).add(day).add(day),
-        rating: '3',
-        status: 'applied',
-        status_sort_index: 2,
-      });
-    })
-    .then(function () { // Inserts seed entries one by one in series
-      return knex('jobs').insert({
-        id: '103',
-        accountId: 'SORT',
-        title: 'D-',
-        employer: 'D-',
-        created_at: moment().subtract(day).subtract(day).subtract(day).subtract(day),
-        updated_at: moment().subtract(day).subtract(day).subtract(day).subtract(day),
-        deadline: moment().add(day).add(day).add(day).add(day),
-        rating: '4',
-        status: 'result',
-        status_sort_index: 3,
-      });
+      return knex('jobs').insert([
+        {
+          id: '100',
+          accountId: 'SORT',
+          title: 'A-',
+          employer: 'A-',
+          created_at: moment().subtract(oneDay),
+          updated_at: moment().subtract(oneDay),
+          deadline: moment().add(oneDay),
+          rating: '1',
+          status: 'interested',
+          status_sort_index: 0,
+        },
+        {
+          id: '101',
+          accountId: 'SORT',
+          title: 'B-',
+          employer: 'B-',
+          created_at: moment().subtract(twoDays),
+          updated_at: moment().subtract(twoDays),
+          deadline: moment().add(twoDays),
+          rating: '2',
+          status: 'applied',
+          status_sort_index: 1,
+        },
+        {
+          id: '102',
+          accountId: 'SORT',
+          title: 'C-',
+          employer: 'C-',
+          created_at: moment().subtract(threeDays),
+          updated_at: moment().subtract(threeDays),
+          deadline: moment().add(threeDays),
+          rating: '3',
+          status: 'applied',
+          status_sort_index: 2,
+        },
+        {
+          id: '103',
+          accountId: 'SORT',
+          title: 'D-',
+          employer: 'D-',
+          created_at: moment().subtract(fourDays),
+          updated_at: moment().subtract(fourDays),
+          deadline: moment().add(fourDays),
+          rating: '4',
+          status: 'result',
+          status_sort_index: 3,
+        },
+      ]);
     });
 };
