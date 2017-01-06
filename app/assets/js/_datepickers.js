715b478030dc9f9ae806fc579854362ba3d612f3
nathancashmore <nathancashmore@ucds.email>
2017-01-06 11:04:32 +0000

    Revert "Replace date-picker component based on SNYK highlighted vulnerabilities and lack of reasonable jquery-ui options.  Remove jquery-ui from project."
    This reverts commit 895e41c57c3058a74f375b6acf3ed0b8ab4ba0ce.
    
    # Conflicts:
    #	npm-shrinkwrap.json
    #	package.json
    
    Address conflicts

--- app/assets/js/_datepickers.js
+++ app/assets/js/_datepickers.js
@@ -1,6 +1,6 @@
 const MobileDetect = require('mobile-detect');
 const $ = require('jquery');
-const Pikaday = require('pikaday');
+require('jquery-ui/datepicker');
 
 const md = new MobileDetect(window.navigator.userAgent);
 const enableNativeDatepicker = md.mobile() || md.tablet();
@@ -10,6 +10,5 @@ if (enableNativeDatepicker) {
     this.type = 'date';
   });
 } else {
-  // eslint-disable-next-line no-new
-  new Pikaday({ field: document.getElementsByClassName('datepicker')[0], format: 'DD/MM/YYYY' });
+  $('.datepicker').datepicker({ dateFormat: 'dd/mm/yy' });
 }
