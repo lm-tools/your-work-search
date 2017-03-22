1958b96617113235a28269f0238c9a592c93c94a
Nathan Cashmore <nathancashmore@digital.uc.dwp.gov.uk>
2017-03-22 10:44:04 +0000

    Remove Introduction Page and change entrypoint
    * Entrypoint will point to dashboard if you have a job and add job if you don't
    * Remove introduction page

--- test/common/page_objects/introduction-page.js
+++
@@ -1,9 +0,0 @@
-const IntroductionPage = function IntroductionPage(browser) {
-  this.browser = browser;
-
-  this.visit = (accountId) => this.browser.visit(`/${accountId}/introduction`);
-  this.clickNext = () => this.browser.click('[data-test="next"]');
-  this.browserPath = () => browser.location.pathname;
-};
-
-module.exports = IntroductionPage;
