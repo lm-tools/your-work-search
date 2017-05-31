00609d0019e947b111281579e5405e3a7856ffc2
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-05-31 11:14:50 +0100

    Text timeline rules for interested

---
+++ app/rules/timeline-rules-text.js
@@ -0,0 +1,84 @@
+const moment = require('moment');
+const _ = require('lodash');
+
+const weekFromNow = () => moment().add(7, 'days');
+const now = () => moment();
+
+function groupByStatus(jobList) {
+  return _.groupBy(jobList, 'status');
+}
+
+function noJobWithStatusDateSet(jobs) {
+  return !jobs.find(it => !!it.statusDate);
+}
+
+function atLeastOneJob(jobs) {
+  return !!jobs && jobs.length > 0;
+}
+
+
+function mostRecentUpdatedDate(jobs) {
+  const sorted = jobs.sort((a, b) => moment(a.updated_at).isBefore(moment(b.updated_at)));
+  return sorted[0].updated_at;
+}
+
+function allJobsMoreThenAWeekInTheFuture(jobs) {
+  return !jobs.find(it => moment(it.statusDate).isSameOrBefore(weekFromNow(), 'day'));
+}
+
+function findJobsExpiringInLessThanAWeek(jobs) {
+  return jobs.filter(
+    it => moment(it.statusDate).isBetween(now(), weekFromNow(), 'day', '[]')
+  );
+}
+
+function getTextForInterested(jobs) {
+  if (noJobWithStatusDateSet(jobs) || allJobsMoreThenAWeekInTheFuture(jobs)) {
+    const date = mostRecentUpdatedDate(jobs);
+    return `Updated ${moment(date).fromNow()}`;
+  }
+
+  const jobsExpiringSoonCount = findJobsExpiringInLessThanAWeek(jobs).length;
+  if (jobsExpiringSoonCount === 1) {
+    return 'One job expiring soon';
+  }
+  if (jobsExpiringSoonCount > 1) {
+    return `${jobsExpiringSoonCount} jobs expiring soon`;
+  }
+  return '';
+}
+
+function rulesForInterested(jobs) {
+  if (atLeastOneJob(jobs)) {
+    const interestedText = getTextForInterested(jobs);
+    return { interested: [interestedText] };
+  }
+  return { interested: [] };
+}
+
+function rulesForApplied(jobs) {
+  return jobs;
+}
+
+function rulesForInterview(jobs) {
+  return jobs;
+}
+
+function rulesForSuccess(jobs) {
+  return jobs;
+}
+
+function text(jobList) {
+  const jobsByStatus = groupByStatus(jobList);
+
+  const rulesPerStatus = Object.assign({},
+    rulesForInterested(jobsByStatus.interested),
+    rulesForApplied(jobsByStatus.applied),
+    rulesForInterview(jobsByStatus.interview),
+    rulesForSuccess(jobsByStatus.success)
+  );
+
+  return rulesPerStatus;
+}
+
+module.exports = { text };
---
+++ test/unit/timeline-rules-text.spec.js
@@ -0,0 +1,102 @@
+const expect = require('chai').expect;
+const moment = require('moment');
+const rules = require('../../app/rules/timeline-rules-text');
+const Job = require('../../app/models/job-model');
+const progression = require('../../app/models/progression');
+
+describe('Timeline rules', function () {
+  const in8days = moment().add(8, 'days').toDate();
+  const in7days = moment().add(7, 'days').toDate();
+  const now = new Date();
+  const yesterday = moment().subtract(1, 'day').toDate();
+
+  function aJob(opts) {
+    return new Job(Object.assign(
+      { status_sort_index: progression.getById(opts.status).order },
+      opts
+    ));
+  }
+
+  describe('text', function () {
+    describe('interested', () => {
+      [
+        { name: 'empty for empty list', jobs: [], result: [] },
+        {
+          name: 'updated date when there is a job without date',
+          jobs: [
+            aJob({ status: 'interested', updated_at: yesterday }),
+            aJob({ status: 'interested', updated_at: now }),
+          ],
+          result: ['Updated a few seconds ago'],
+        },
+        {
+          name: 'updated date when every input date is more then 7 days in the future',
+          jobs: [
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: yesterday }),
+          ],
+          result: ['Updated a few seconds ago'],
+        },
+        {
+          name: 'updated date when one date is 7 days in the future and rest are after',
+          jobs: [
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in7days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+          ],
+          result: ['One job expiring soon'],
+        },
+        {
+          name: 'updated date when one date expires in less then a week',
+          jobs: [
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in7days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+          ],
+          result: ['One job expiring soon'],
+        },
+        {
+          name: 'updated date when few dates expires in less then a week',
+          jobs: [
+            aJob({ status: 'interested', deadlineDate: in7days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in7days, updated_at: now }),
+            aJob({ status: 'interested', deadlineDate: in8days, updated_at: now }),
+          ],
+          result: ['2 jobs expiring soon'],
+        },
+
+      ].forEach(s => {
+        it(s.name, () => {
+          expect(rules.text(s.jobs).interested).to.eql(s.result);
+        });
+      });
+    });
+
+    describe('applied', () => {
+      [
+      ].forEach(s => {
+        it(s.name, () => {
+          expect(rules.text(s.jobs).applied).to.eql(s.result);
+        });
+      });
+    });
+
+    describe('interview', () => {
+      [
+      ].forEach(s => {
+        it(s.name, () => {
+          expect(rules.text(s.jobs).interview).to.eql(s.result);
+        });
+      });
+    });
+
+    describe('success', () => {
+      [
+      ].forEach(s => {
+        it(s.name, () => {
+          expect(rules.text(s.jobs).success).to.eql(s.result);
+        });
+      });
+    });
+  });
+});
