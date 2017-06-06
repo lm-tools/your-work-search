e6412269faab5c6027c1fbfd433715ab3d08f64b
nathancashmoredwp <nathancashmore@ucds.email>
2017-06-06 14:59:12 +0100

    Move dashboard assets around and display static timeline in situ.

--- app/app.js
+++ app/app.js
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
       googleTagManager: 'partials/google-tag-manager',
       progressionStatusDates: 'partials/progression-status-dates',
       timeline: 'partials/timeline',
+      sort: 'partials/sort',
     },
   });
   next();
